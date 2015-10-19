c8285af1bda639ec6b98c337f625a9223a75bb51
Andreas W√ºrl <andi@tryb.de>
2015-10-19 20:13:03 +0200

    Initial Commit

---
+++ monitor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  monitor
+//
+//  Created by Andreas Würl on 19.10.15.
+//  Copyright © 2015 Andreas Würl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
